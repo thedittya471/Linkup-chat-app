703b80a133a84ff734c91fa45229e8b9f97d4ce3
thedittya471 <dittyamaity@gmail.com>
2026-03-25 16:44:44 +0530

    Refactor backend structure: create app and db connection modules

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
-import express from "express";
+import app from './src/app.js';
+import dotenv from 'dotenv';
+import connectDB from './src/common/config/db.js';
 
-const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+dotenv.config({
+    path: './.env'
 });
+
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+    await connectDB();
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode`);
+    });
+};
+
+startServer().catch((error) => {
+    console.error('Error starting server:', error);
+    process.exit(1);
+}); 
